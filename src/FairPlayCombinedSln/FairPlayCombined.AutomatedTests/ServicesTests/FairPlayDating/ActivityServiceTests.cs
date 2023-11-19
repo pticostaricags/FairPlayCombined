55b5a80bbe5124ec28de72c7bd213ea87e25158d
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-18 20:00:00 -0600

    Adding test for auto generated method

---
+++ src/FairPlayCombinedSln/FairPlayCombined.AutomatedTests/ServicesTests/FairPlayDating/ActivityServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using FairPlayCombined.DataAccess.Data;
+using FairPlayCombined.Models.FairPlayDating;
+using FairPlayCombined.Services.FairPlayDating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace FairPlayCombined.AutomatedTests.ServicesTests.FairPlayDating
+{
+    [TestClass]
+    public class ActivityServiceTests
+    {
+        public static readonly MsSqlContainer _msSqlContainer
+        = new MsSqlBuilder().Build();
+        [ClassInitialize]
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static async Task ClassInitializeAsync(TestContext testContext)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            await _msSqlContainer.StartAsync();
+        }
+
+        [ClassCleanup()]
+        public static async Task ClassCleanupAsync()
+        {
+            if (_msSqlContainer.State == DotNet.Testcontainers.Containers.TestcontainersStates.Running)
+            {
+                await _msSqlContainer.StopAsync();
+            }
+        }
+
+        [TestMethod]
+        public async Task Test_CreateActivityAsync()
+        {
+            ServiceCollection services = new ServiceCollection();
+            var cs = _msSqlContainer.GetConnectionString();
+            services.AddDbContextFactory<FairPlayCombinedDbContext>(
+                optionsAction => 
+                {
+                    optionsAction.UseSqlServer(cs);
+                });
+            services.AddTransient<ActivityService>();
+            var sp = services.BuildServiceProvider();
+            var dbContext = sp.GetRequiredService<FairPlayCombinedDbContext>();
+            await dbContext.Database.EnsureCreatedAsync();
+            var activityService = sp.GetRequiredService<ActivityService>();
+            CreateActivityModel createActivityModel = new CreateActivityModel()
+            {
+                Name = "TestModel"
+            };
+            await activityService.CreateActivityModelAsync(createActivityModel, CancellationToken.None);
+            var result = await dbContext.Activity.SingleOrDefaultAsync();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(createActivityModel.Name, result.Name);
+        }
+    }
+}
