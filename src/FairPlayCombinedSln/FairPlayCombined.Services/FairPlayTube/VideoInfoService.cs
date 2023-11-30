597d45749c43fc78c303eaa4f80b1662f0ed9ff4
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-30 07:54:33 -0600

    Retrieving only videos in processed status

--- src/FairPlayCombinedSln/FairPlayCombined.Services/FairPlayTube/VideoInfoService.cs
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/FairPlayTube/VideoInfoService.cs
@@ -3,11 +3,8 @@ using FairPlayCombined.DataAccess.Data;
 using FairPlayCombined.DataAccess.Models.FairPlayTubeSchema;
 using FairPlayCombined.Models.FairPlayTube.VideoInfo;
 using FairPlayCombined.Models.Pagination;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
 
 namespace FairPlayCombined.Services.FairPlayTube
 {
@@ -22,5 +19,52 @@ namespace FairPlayCombined.Services.FairPlayTube
         >]
     public partial class VideoInfoService : BaseService
     {
+        public async Task<PaginationOfT<VideoInfoModel>> GetPaginatedCompletedVideoInfoAsync(
+    PaginationRequest paginationRequest,
+    CancellationToken cancellationToken
+    )
+        {
+            PaginationOfT<VideoInfoModel> result = new();
+            var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            string orderByString = string.Empty;
+            if (paginationRequest.SortingItems?.Length > 0)
+                orderByString =
+                    String.Join(",",
+                    paginationRequest.SortingItems.Select(p => $"{p.PropertyName} {GetSortTypeString(p.SortType)}"));
+            var query = dbContext.VideoInfo
+                .Where(p=>p.VideoIndexStatusId == (short)FairPlayCombined.Common.FairPlayTube.Enums.VideoIndexStatus.Processed)
+                .Select(p => new VideoInfoModel
+                {
+                    VideoInfoId = p.VideoInfoId,
+                    AccountId = p.AccountId,
+                    VideoId = p.VideoId,
+                    Location = p.Location,
+                    Name = p.Name,
+                    Description = p.Description,
+                    FileName = p.FileName,
+                    VideoBloblUrl = p.VideoBloblUrl,
+                    IndexedVideoUrl = p.IndexedVideoUrl,
+                    ApplicationUserId = p.ApplicationUserId,
+                    VideoIndexStatusId = p.VideoIndexStatusId,
+                    VideoDurationInSeconds = p.VideoDurationInSeconds,
+                    VideoIndexSourceClass = p.VideoIndexSourceClass,
+                    Price = p.Price,
+                    ExternalVideoSourceUrl = p.ExternalVideoSourceUrl,
+                    VideoLanguageCode = p.VideoLanguageCode,
+                    VideoVisibilityId = p.VideoVisibilityId,
+                    ThumbnailUrl = p.ThumbnailUrl,
+
+                });
+            if (!String.IsNullOrEmpty(orderByString))
+                query = query.OrderBy(orderByString);
+            result.TotalItems = await query.CountAsync(cancellationToken);
+            result.PageSize = paginationRequest.PageSize;
+            result.TotalPages = (int)Math.Ceiling((decimal)result.TotalItems / result.PageSize);
+            result.Items = await query
+            .Skip(paginationRequest.StartIndex)
+            .Take(paginationRequest.PageSize)
+            .ToArrayAsync(cancellationToken);
+            return result;
+        }
     }
 }
