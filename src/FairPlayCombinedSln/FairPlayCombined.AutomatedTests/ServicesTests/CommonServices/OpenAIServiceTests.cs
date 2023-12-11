52afd5866211e234af6a5dca422fc10a07b49099
Eduardo Fonseca <efonsecab@gmail.com>
2023-12-11 17:56:49 -0600

    Adding ability to generate images using Open AI dall-e-3

---
+++ src/FairPlayCombinedSln/FairPlayCombined.AutomatedTests/ServicesTests/CommonServices/OpenAIServiceTests.cs
@@ -0,0 +1,38 @@
+﻿#if Debug_Enable_Paid_Tests
+using FairPlayCombined.Services.Common;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.AutomatedTests.ServicesTests.CommonServices
+{
+    [TestClass]
+    public class OpenAIServiceTests: ServicesBase
+    {
+        [TestMethod]
+        public async Task Test_GenerateDallE3ImageAsync()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddUserSecrets<ServicesBase>();
+            var configuration = configurationBuilder.Build();
+            var openAIKey = configuration["OpenAIKey"] ??
+                throw new Exception("'OpenAIKey' is not in configuration");
+            var generateDall3ImageUrl = configuration["GenerateDall3ImageUrl"] ??
+                throw new Exception("'GenerateDall3ImageUrl' is not in configuration");
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+                openAIKey);
+            OpenAIServiceConfiguration openAIServiceConfiguration = new()
+            {
+                GenerateDall3ImageUrl = generateDall3ImageUrl
+            };
+            OpenAIService openAIService =new(httpClient, openAIServiceConfiguration);
+            var result = await openAIService.GenerateDallE3ImageAsync(prompt: "logo for a Social Network app named FairPlaySocial", cancellationToken: CancellationToken.None);
+            Assert.IsNotNull(result);
+        }
+    }
+}
+#endif
---
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/Common/OpenAIService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.Services.Common;
+public class OpenAIService(HttpClient httpClient, OpenAIServiceConfiguration openAIServiceConfiguration)
+{
+
+    public async Task<GenerateDallE3ResponseModel?> GenerateDallE3ImageAsync(string prompt, CancellationToken cancellationToken)
+    {
+        var requestUrl = openAIServiceConfiguration.GenerateDall3ImageUrl;
+        GenerateDallE3RequestModel requestModel = new()
+        {
+            model = "dall-e-3",
+            prompt = prompt,
+            n = 1,
+            size = "1024x1024"
+        };
+        var response = await httpClient.PostAsJsonAsync<GenerateDallE3RequestModel>(requestUrl,
+            requestModel, cancellationToken: cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadFromJsonAsync<GenerateDallE3ResponseModel>();
+        return result;
+
+    }
+}
+
+public class OpenAIServiceConfiguration
+{
+    public string? GenerateDall3ImageUrl { get; set; }
+}
+
+
+public class GenerateDallE3RequestModel
+{
+    public string? model { get; set; }
+    public string? prompt { get; set; }
+    public int n { get; set; }
+    public string? size { get; set; }
+}
+
+
+
+public class GenerateDallE3ResponseModel
+{
+    public int created { get; set; }
+    public GenerateDallE3ResponseDatumModel[]? data { get; set; }
+}
+
+public class GenerateDallE3ResponseDatumModel
+{
+    public string? revised_prompt { get; set; }
+    public string? url { get; set; }
+}
