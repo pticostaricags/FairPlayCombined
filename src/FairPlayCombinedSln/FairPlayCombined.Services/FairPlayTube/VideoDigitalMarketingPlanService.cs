bd632dbdd397890a6754a60db5221369c4d06db3
Eduardo Fonseca <efonsecab@gmail.com>
2023-12-15 16:46:11 -0600

    Adding ability to recreate marketing plan

--- src/FairPlayCombinedSln/FairPlayCombined.Services/FairPlayTube/VideoDigitalMarketingPlanService.cs
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/FairPlayTube/VideoDigitalMarketingPlanService.cs
@@ -16,24 +16,31 @@ namespace FairPlayCombined.Services.FairPlayTube
             string socialNetworkName,
             CancellationToken cancellationToken)
         {
-            var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken:cancellationToken);
+            var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken: cancellationToken);
             var result = await dbContext.VideoDigitalMarketingPlan.Where(
                 p => p.VideoInfoId == videoInfoId && p.SocialNetworkName == socialNetworkName)
                 .Select(p => p.HtmlDigitalMarketingPlan)
                 .SingleOrDefaultAsync(cancellationToken: cancellationToken);
             return result;
         }
-        public async Task SaveVideoDigitalMarketingPlanAsync(long videoInfoId, 
+        public async Task SaveVideoDigitalMarketingPlanAsync(long videoInfoId,
             string htmlDigitalMarketingPlan,
             string socialNetworkName,
+            bool replaceExistent,
             CancellationToken cancellationToken)
         {
             var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken: cancellationToken);
-            await dbContext.VideoDigitalMarketingPlan.AddAsync(new() 
+            if (replaceExistent)
+            {
+                await dbContext.VideoDigitalMarketingPlan
+                    .Where(p => p.VideoInfoId == videoInfoId)
+                    .ExecuteDeleteAsync(cancellationToken: cancellationToken);
+            }
+            await dbContext.VideoDigitalMarketingPlan.AddAsync(new()
             {
                 VideoInfoId = videoInfoId,
-                HtmlDigitalMarketingPlan= htmlDigitalMarketingPlan,
-                SocialNetworkName= socialNetworkName
+                HtmlDigitalMarketingPlan = htmlDigitalMarketingPlan,
+                SocialNetworkName = socialNetworkName
             },
                 cancellationToken: cancellationToken);
             await dbContext.SaveChangesAsync(cancellationToken: cancellationToken);
