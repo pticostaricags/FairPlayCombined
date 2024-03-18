3670249b5399ce7826991c8f3a5a87cf3c7941b6
Eduardo Fonseca <efonsecab@gmail.com>
2024-03-18 07:39:19 -0600

    Adding base for ThemeConfiguration

---
+++ src/FairPlayCombinedSln/FairPlayCombined.Models/ThemeConfiguration/ThemeConfigurationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.Models.ThemeConfiguration
+{
+    public class ThemeConfigurationModel
+    {
+        [Required]
+        public string? Key { get; set; }
+        [Required]
+        public string? Value { get; set; }
+    }
+}
