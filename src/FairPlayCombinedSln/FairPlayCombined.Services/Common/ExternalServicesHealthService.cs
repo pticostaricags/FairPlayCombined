19fbee6dabe4e52a49e42f3e921402c89c8c4112
Eduardo Fonseca <efonsecab@gmail.com>
2024-03-23 14:46:05 +0000

    Adding base for checking configuration health

---
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/Common/ExternalServicesHealthService.cs
@@ -0,0 +1,61 @@
+﻿using Azure.AI.OpenAI;
+using FairPlayCombined.DataAccess.Models.FairPlayTubeSchema;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FairPlayCombined.Services.Common.AzureOpenAIService;
+
+namespace FairPlayCombined.Services.Common
+{
+    public class ExternalServicesHealthService(ExternalServicesConfigurationModel externalServicesConfigurationModel)
+    {
+        public async Task<ExternalServicesHealthModel[]> CheckServicesHealthAsync(
+            CancellationToken cancellationToken)
+        {
+            ExternalServicesHealthModel azureOpenAIHealth = new();
+            try
+            {
+                string systemMessage = "Reply with 'YES'";
+                ChatCompletionsOptions chatCompletionsOptions = new()
+                {
+                    DeploymentName = AzureOpenAIService.DeploymentName,
+                    Messages =
+                {
+                    new ChatMessage(ChatRole.System, systemMessage)
+                }
+                };
+                var response = await externalServicesConfigurationModel.OpenAIClient!.GetChatCompletionsAsync(
+                chatCompletionsOptions, cancellationToken: cancellationToken);
+                var contentResponse =
+                response.Value.Choices[0].Message.Content;
+                azureOpenAIHealth.Response = contentResponse;
+                azureOpenAIHealth.IsHealthy = true;
+            }
+            catch (Exception ex)
+            {
+                azureOpenAIHealth.Response = ex.ToString();
+                azureOpenAIHealth.IsHealthy = false;
+            }
+            return [azureOpenAIHealth];
+        }
+    }
+
+    public class ExternalServicesConfigurationModel
+    {
+        public Azure.AI.OpenAI.OpenAIClient? OpenAIClient { get; set; }
+        public ContentModeratorClient? ContentModeratorClient
+        {
+            get; set;
+        }
+    }
+
+    public class ExternalServicesHealthModel
+    {
+        public string? ServiceName { get; set; }
+        public bool IsHealthy { get; set; }
+        public string? Response { get; set; }
+    }
+}
