9ced3f06cf34a27ab579169c5e2e053b0ba0100b
Eduardo Fonseca <efonsecab@gmail.com>
2024-03-27 10:43:22 -0600

    removing duplications

--- src/FairPlayCombinedSln/FairPlayDating/Program.cs
+++ src/FairPlayCombinedSln/FairPlayDating/Program.cs
@@ -82,25 +82,6 @@ builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddSingleton<AzureOpenAIServiceConfiguration>(sp =>
-{
-    var dbContextFactory = sp.GetRequiredService<IDbContextFactory<FairPlayCombinedDbContext>>();
-    var dbContext = dbContextFactory.CreateDbContext();
-    var azureOpenAIEndpointEntity = dbContext.ConfigurationSecret.SingleOrDefault(p => p.Name ==
-    Constants.ConfigurationSecretsKeys.AZURE_OPENAI_ENDPOINT_KEY) ?? throw new InvalidOperationException($"Unable to find {nameof(ConfigurationSecret)} = {Constants.ConfigurationSecretsKeys.AZURE_OPENAI_ENDPOINT_KEY} in database");
-    var azureOpenAIKeyEntity = dbContext.ConfigurationSecret.SingleOrDefault(p => p.Name ==
-    Constants.ConfigurationSecretsKeys.AZURE_OPENAI_KEY_KEY) ?? throw new InvalidOperationException($"Unable to find {nameof(ConfigurationSecret)} = {Constants.ConfigurationSecretsKeys.AZURE_OPENAI_KEY_KEY} in database");
-    var azureOpenAIDeploymentNameEntity = dbContext.ConfigurationSecret.SingleOrDefault(p => p.Name ==
-    Constants.ConfigurationSecretsKeys.AZURE_OPENAI_DEPLOYMENT_NAME_KEY) ?? throw new InvalidOperationException($"Unable to find {nameof(ConfigurationSecret)} = {Constants.ConfigurationSecretsKeys.AZURE_OPENAI_DEPLOYMENT_NAME_KEY} in database");
-    AzureOpenAIServiceConfiguration azureOpenAIServiceConfiguration = new()
-    {
-        DeploymentName = azureOpenAIDeploymentNameEntity.Value,
-        Endpoint = azureOpenAIEndpointEntity.Value,
-        Key = azureOpenAIKeyEntity.Value
-    };
-    return azureOpenAIServiceConfiguration;
-});
 builder.Services.AddAzureOpenAIService();
 
 builder.Services.AddTransient<ContentModeratorClient>(sp =>
