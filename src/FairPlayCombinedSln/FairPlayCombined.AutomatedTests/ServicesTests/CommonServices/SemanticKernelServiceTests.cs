0aa4391ffc637eea86462ea9a982a6e54e880ceb
Eduardo Fonseca <efonsecab@gmail.com>
2024-03-27 12:15:05 +0000

    Adding base for Semantic Kernel

---
+++ src/FairPlayCombinedSln/FairPlayCombined.AutomatedTests/ServicesTests/CommonServices/SemanticKernelServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using FairPlayCombined.Services.AI;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.AutomatedTests.ServicesTests.CommonServices
+{
+    [TestClass]
+    public class SemanticKernelServiceTests: ServicesBase
+    {
+        [TestMethod]
+        public async Task Test_TranslateTextAsync()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddUserSecrets<ServicesBase>();
+            var configuration = configurationBuilder.Build();
+            var endpoint = configuration["AzureOpenAI:Endpoint"] ??
+                throw new Exception("'AzureOpenAI:Endpoint' is not in configuration");
+            var key = configuration["AzureOpenAI:Key"] ??
+                throw new Exception("'AzureOpenAI:Key' is not in configuration");
+            var deploymentName = configuration["AzureOpenAI:DeploymentName"] ??
+                throw new Exception("'AzureOpenAI:DeploymentName' is not in configuration");
+            AzureOpenAIConfiguration azureOpenAIConfiguration = new()
+            {
+                DeploymentName = deploymentName,
+                Endpoint = endpoint,
+                Key = key
+            };
+            SemanticKernelService semanticKernelService = new(azureOpenAIConfiguration);
+            var result = await semanticKernelService.TranslateTextAsync("This is a test",
+                fromLanguage: "en-US", toLanguage: "es-CR",
+                cancellationToken:CancellationToken.None);
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ src/FairPlayCombinedSln/FairPlayCombined.Services.AI/SemanticKernelService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace FairPlayCombined.Services.AI
+{
+    public class SemanticKernelService(AzureOpenAIConfiguration azureOpenAIConfiguration)
+    {
+        public async Task<FunctionResult?> TranslateTextAsync(string text, string fromLanguage, string toLanguage,
+            CancellationToken cancellationToken)
+        {
+            var builder = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(deploymentName:azureOpenAIConfiguration.DeploymentName!,
+                endpoint:azureOpenAIConfiguration.Endpoint!, apiKey:azureOpenAIConfiguration.Key!);
+            var kernel = builder.Build();
+            string promptTemplate = @$"Translate text between languages. 
+From Language: {{${fromLanguage}}}. 
+To Language: {{${toLanguage}}}.
+Text: {{${text}}}.";
+            var function = kernel.CreateFunctionFromPrompt(promptTemplate);
+            var result = await kernel.InvokeAsync(function, new() 
+            {
+                [nameof(fromLanguage)] = fromLanguage,
+                [nameof(toLanguage)] = toLanguage,
+                [nameof(text)] = text
+            }, cancellationToken:cancellationToken);
+            return result;
+        }
+    }
+
+    public class AzureOpenAIConfiguration
+    {
+        public string? DeploymentName { get; set; }
+        public string? Endpoint { get; set; }
+        public string? Key { get; set; }
+    }
+}
