ecc2d008a95a8a4f32c9da3cc96db0254624fe87
Eduardo Fonseca <efonsecab@gmail.com>
2024-04-03 23:05:28 +0000

    Adding google gemini

---
+++ src/FairPlayCombinedSln/FairPlayCombined.Models/GoogleGemini/GenerateContenRequestModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.Models.GoogleGemini
+{
+
+    public class GenerateContentRequestModel
+    {
+        public Content[]? contents { get; set; }
+        public Generationconfig? generationConfig { get; set; }
+        public Safetysetting[]? safetySettings { get; set; }
+
+        private const string SAFETY_SETTINGS_CATEGORY_HARASSMENT = "HARM_CATEGORY_HARASSMENT";
+        private const string SAFETY_SETTINGS_TRESHOLD_MEDIUM_AND_ABOVE = "BLOCK_MEDIUM_AND_ABOVE";
+        private const string SAFETY_SETTINGS_CATEGORY_HATE_SPEECH = "HARM_CATEGORY_HATE_SPEECH";
+        private const string SAFET_SETTINGS_CATEGORY_DANGEROUS_CONTENT = "HARM_CATEGORY_DANGEROUS_CONTENT";
+
+        public static readonly GenerateContentRequestModel DefaultGenerateContentRequestModel =
+            new()
+            {
+                generationConfig=new()
+                {
+                    temperature = 0.9f,
+                    topK=1,
+                    topP=1,
+                    maxOutputTokens=2048
+                },
+                safetySettings=new Safetysetting[] 
+                {
+                    new()
+                    {
+                        category = SAFETY_SETTINGS_CATEGORY_HARASSMENT,
+                        threshold = SAFETY_SETTINGS_TRESHOLD_MEDIUM_AND_ABOVE
+                    },
+                    new()
+                    {
+                        category = SAFETY_SETTINGS_CATEGORY_HATE_SPEECH,
+                        threshold = SAFETY_SETTINGS_TRESHOLD_MEDIUM_AND_ABOVE
+                    },
+                    new()
+                    {
+                        category = SAFET_SETTINGS_CATEGORY_DANGEROUS_CONTENT,
+                        threshold = SAFETY_SETTINGS_TRESHOLD_MEDIUM_AND_ABOVE
+                    }
+                }
+            };
+    }
+
+    public class Generationconfig
+    {
+        public float temperature { get; set; }
+        public int topK { get; set; }
+        public int topP { get; set; }
+        public int maxOutputTokens { get; set; }
+        public object[]? stopSequences { get; set; }
+    }
+
+    public class Content
+    {
+        public string? role { get; set; }
+        public Part[]? parts { get; set; }
+    }
+
+    public class Part
+    {
+        public string? text { get; set; }
+    }
+
+    public class Safetysetting
+    {
+        public string? category { get; set; }
+        public string? threshold { get; set; }
+    }
+
+}
---
+++ src/FairPlayCombinedSln/FairPlayCombined.Models/GoogleGemini/GenerateContentResponseModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.Models.GoogleGemini
+{
+
+    public class GenerateContentResponseModel
+    {
+        public GenerateContentResponseCandidate[]? candidates { get; set; }
+        public GenerateContentResponsePromptfeedback? promptFeedback { get; set; }
+    }
+
+    public class GenerateContentResponsePromptfeedback
+    {
+        public GenerateContentResponseSafetyrating[]? safetyRatings { get; set; }
+    }
+
+    public class GenerateContentResponseSafetyrating
+    {
+        public string? category { get; set; }
+        public string? probability { get; set; }
+    }
+
+    public class GenerateContentResponseCandidate
+    {
+        public Content? content { get; set; }
+        public string? finishReason { get; set; }
+        public int index { get; set; }
+        public GenerateContentResponseSafetyrating1[]? safetyRatings { get; set; }
+    }
+
+    public class GenerateContentResponseContent
+    {
+        public GenerateContentResponsePart[]? parts { get; set; }
+        public string? role { get; set; }
+    }
+
+    public class GenerateContentResponsePart
+    {
+        public string? text { get; set; }
+    }
+
+    public class GenerateContentResponseSafetyrating1
+    {
+        public string? category { get; set; }
+        public string? probability { get; set; }
+    }
+
+}
