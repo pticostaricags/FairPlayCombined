baf16685a4c1089ea9edca2db83de44342564021
Eduardo Fonseca <efonsecab@gmail.com>
2024-04-11 12:07:49 +0000

    enabling basic support for analyzing watch time

--- src/FairPlayCombinedSln/FairPlayTube/wwwroot/scripts/videojs.js
+++ src/FairPlayCombinedSln/FairPlayTube/wwwroot/scripts/videojs.js
@@ -1,3 +1,29 @@
+let timer = null;
+let totalTime = 0;
+let lastPlayerTime = null;
+let player = null;
 function initializeVideoJsPlayer(playerElementId) {
-    videojs(playerElementId, {});
+    debugger;
+    player = videojs(playerElementId, {});
+    lastPlayerTime = player.currentTime();
+    player.on('play', startPlaying);
+    player.on('pause', pausePlaying);
+}
+
+function startPlaying() {
+    console.log('played');
+    timer = window.setInterval(function () {
+        if (lastPlayerTime != player.currentTime()) {
+            totalTime += 1;
+        }
+        lastPlayerTime = player.currentTime();
+        console.log(lastPlayerTime);
+        console.log(player.currentTime());
+        console.log(totalTime);
+    }, 1000);
+}
+
+function pausePlaying() {
+    console.log('stopped');
+    if (timer) clearInterval(timer);
 }
