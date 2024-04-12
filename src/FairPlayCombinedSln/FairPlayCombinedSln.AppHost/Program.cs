9c63810e8c29cb033c4ea78dda63550ded65f20a
Eduardo Fonseca <efonsecab@gmail.com>
2024-04-12 08:03:44 -0600

    updating google auth setup for deployments

--- src/FairPlayCombinedSln/FairPlayCombinedSln.AppHost/Program.cs
+++ src/FairPlayCombinedSln/FairPlayCombinedSln.AppHost/Program.cs
@@ -2,18 +2,27 @@ using Microsoft.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 builder.Configuration.AddUserSecrets<Program>();
-string googleAuthClientInfo = string.Empty;
-try
-{
-    var googleAuthClientSecretsFilePath = builder.Configuration["GoogleAuthClientSecretsFilePath"] ??
-        throw new InvalidOperationException("'GoogleAuthClientSecretsFilePath' not found");
-    googleAuthClientInfo = System.IO.File.ReadAllText(googleAuthClientSecretsFilePath);
-}
-catch (Exception)
-{
-    googleAuthClientInfo = builder.Configuration["GoogleAuthClientSecretsFileInfo"] ??
-        throw new InvalidOperationException("'GoogleAuthClientSecretsFileInfo' not found");
-}
+
+var googleAuthClientId = builder.Configuration["GoogleAuthClientId"] ??
+        throw new InvalidOperationException("'GoogleAuthClientId' not found");
+
+var googleAuthProjectId = builder.Configuration["GoogleAuthProjectId"] ??
+        throw new InvalidOperationException("'GoogleAuthProjectId' not found");
+
+var googleAuthUri = builder.Configuration["GoogleAuthUri"] ??
+        throw new InvalidOperationException("'GoogleAuthUri' not found");
+
+var googleAuthTokenUri = builder.Configuration["GoogleAuthTokenUri"] ??
+        throw new InvalidOperationException("'GoogleAuthTokenUri' not found");
+
+var googleAuthProviderCertUri = builder.Configuration["GoogleAuthProviderCertUri"] ??
+        throw new InvalidOperationException("'GoogleAuthProviderCertUri' not found");
+
+var googleAuthClientSecret = builder.Configuration["GoogleAuthClientSecret"] ??
+        throw new InvalidOperationException("'GoogleAuthClientSecret' not found");
+
+var googleAuthRedirectUri = builder.Configuration["GoogleAuthRedirectUri"] ??
+        throw new InvalidOperationException("'GoogleAuthRedirectUri' not found");
 
 IResourceBuilder<IResourceWithConnectionString> sqlResourceWithConnectionString =
     builder.AddConnectionString("FairPlayCombinedDb");
@@ -35,7 +44,13 @@ if (addFairPlayTube)
     builder.AddProject<Projects.FairPlayTube>(nameof(Projects.FairPlayTube).ToLower())
     .WithEnvironment(callback =>
     {
-        callback.EnvironmentVariables.Add("GoogleAuthClientInfo", googleAuthClientInfo);
+        callback.EnvironmentVariables.Add("GoogleAuthClientId", googleAuthClientId);
+        callback.EnvironmentVariables.Add("GoogleAuthProjectId", googleAuthProjectId);
+        callback.EnvironmentVariables.Add("GoogleAuthUri", googleAuthUri);
+        callback.EnvironmentVariables.Add("GoogleAuthTokenUri", googleAuthTokenUri);
+        callback.EnvironmentVariables.Add("GoogleAuthProviderCertUri", googleAuthProviderCertUri);
+        callback.EnvironmentVariables.Add("GoogleAuthClientSecret", googleAuthClientSecret);
+        callback.EnvironmentVariables.Add("GoogleAuthRedirectUri", googleAuthRedirectUri);
     })
     .WithReference(sqlResourceWithConnectionString);
     builder.AddProject<Projects.FairPlayTube_VideoIndexing>("fairplaytubevideoindexing")
--- src/FairPlayCombinedSln/FairPlayTube/Program.cs
+++ src/FairPlayCombinedSln/FairPlayTube/Program.cs
@@ -40,10 +40,40 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-var googleAuthClientInfoJson = Environment.GetEnvironmentVariable("GoogleAuthClientInfo") ??
-    throw new InvalidOperationException("'GoogleAuthClientInfo' not found");
-GoogleAuthClientSecretInfo googleAuthClientSecretInfo = 
-    System.Text.Json.JsonSerializer.Deserialize<GoogleAuthClientSecretInfo>(googleAuthClientInfoJson)!;
+var googleAuthClientId = builder.Configuration["GoogleAuthClientId"] ??
+        throw new InvalidOperationException("'GoogleAuthClientId' not found");
+
+var googleAuthProjectId = builder.Configuration["GoogleAuthProjectId"] ??
+        throw new InvalidOperationException("'GoogleAuthProjectId' not found");
+
+var googleAuthUri = builder.Configuration["GoogleAuthUri"] ??
+        throw new InvalidOperationException("'GoogleAuthUri' not found");
+
+var googleAuthTokenUri = builder.Configuration["GoogleAuthTokenUri"] ??
+        throw new InvalidOperationException("'GoogleAuthTokenUri' not found");
+
+var googleAuthProviderCertUri = builder.Configuration["GoogleAuthProviderCertUri"] ??
+        throw new InvalidOperationException("'GoogleAuthProviderCertUri' not found");
+
+var googleAuthClientSecret = builder.Configuration["GoogleAuthClientSecret"] ??
+        throw new InvalidOperationException("'GoogleAuthClientSecret' not found");
+
+var googleAuthRedirectUri = builder.Configuration["GoogleAuthRedirectUri"] ??
+        throw new InvalidOperationException("'GoogleAuthRedirectUri' not found");
+
+GoogleAuthClientSecretInfo googleAuthClientSecretInfo = new()
+{
+    installed = new Installed()
+    {
+        auth_provider_x509_cert_url = googleAuthProviderCertUri,
+        auth_uri = googleAuthUri,
+        client_id = googleAuthClientId,
+        client_secret = googleAuthClientSecret,
+        project_id = googleAuthProjectId,
+        redirect_uris = [googleAuthRedirectUri],
+        token_uri = googleAuthTokenUri
+    }
+};
 builder.Services.AddSingleton<YouTubeClientServiceConfiguration>(new YouTubeClientServiceConfiguration()
 {
     GoogleAuthClientSecretInfo = googleAuthClientSecretInfo
