48197adb6139678eb6d8516c095e468c7e39952e
Eduardo Fonseca <efonsecab@gmail.com>
2024-05-06 18:16:01 -0600

    Adding base for PayPalOrderService

---
+++ src/FairPlayCombinedSln/FairPlayCombined.AutomatedTests/ServicesTests/CommonServices/PayPalOrderServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FairPlayCombined.Services.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PayPal.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.AutomatedTests.ServicesTests.CommonServices
+{
+    [TestClass]
+    public class PayPalOrderServiceTests: ServicesBase
+    {
+        [TestMethod]
+        public async Task Test_CreateOrderAsync()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddUserSecrets<ServicesBase>();
+            var configuration = configurationBuilder.Build();
+            var clientId = configuration["PayPal:ClientId"]!;
+            var clientSecret = configuration["PayPal:ClientSecret"]!;
+            var sandboxBuyer = configuration["PayPal:SandboxBuyer"]!;
+            SandboxEnvironment sandboxEnvironment = new(clientId, clientSecret);
+            PayPalHttpClient payPalHttpClient = new(sandboxEnvironment);
+            var loggerFactory = LoggerFactory.Create(p => p.AddConsole());
+            var logger = loggerFactory!.CreateLogger<PayPalOrderService>();
+            PayPalOrderService payPalOrderService = new(payPalHttpClient, logger);
+            var result = await payPalOrderService.CreateOrderAsync(
+                "1234", 1.2M, "Automated Tests",
+                returnUrl: "https://example.com/returnUrl",
+                cancelUrl: "https://example.com/returnUrl");
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/Common/PayPalOrderService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using PayPal.Core;
+using PayPal.v1.Orders;
+
+namespace FairPlayCombined.Services.Common
+{
+    public class PayPalOrderService(PayPalHttpClient payPalHttpClient,
+        ILogger<PayPalOrderService> logger)
+    {
+        public async Task<Order?> CreateOrderAsync(string referenceId,
+            decimal amount, string brandName,
+            string returnUrl, string cancelUrl)
+        {
+            try
+            {
+                OrdersCreateRequest ordersCreateRequest = new();
+                Order order = new()
+                {
+                    Intent = "CAPTURE",
+                    PurchaseUnits =
+                    [
+                        new PurchaseUnit()
+                    {
+                        ReferenceId = referenceId,
+                        Amount=new()
+                        {
+                            Currency="USD",
+                            Total = amount.ToString()
+                        }
+                    }
+                    ],
+                    ApplicationContext = new()
+                    {
+                        BrandName = brandName,
+                        Locale = "en-US",
+                    },
+                    RedirectUrls=new RedirectUrls()
+                    {
+                        ReturnUrl= returnUrl,
+                        CancelUrl= cancelUrl
+                    }
+                };
+                ordersCreateRequest.RequestBody(order);
+                var response = await payPalHttpClient.Execute(ordersCreateRequest);
+                var result = response.Result<Order>();
+                return result;
+            }
+            catch (Exception ex) 
+            {
+                logger.LogError(exception: ex, message: "Exception occurred in {MethodName}. " +
+                    "Message: {Message}", nameof(CreateOrderAsync), ex.Message);
+                return null;
+            }
+        }
+    }
+}
