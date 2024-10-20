405035cde1b64dcfb7cf1cf8a498d61c44a6939d
Eduardo Fonseca <efonsecab@gmail.com>
2024-10-20 08:34:57 -0600

    Reducing database calls

--- src/FairPlayCombinedSln/FairPlayCombined.Services/Extensions/OpenAIExtensions.cs
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/Extensions/OpenAIExtensions.cs
@@ -16,7 +16,7 @@ namespace FairPlayCombined.Services.Extensions
     {
         public static void AddOpenAI(this WebApplicationBuilder builder)
         {
-            builder.Services.AddTransient<IOpenAIService, OpenAIService>(sp =>
+            builder.Services.AddTransient<OpenAIServiceConfiguration>(sp => 
             {
                 IDbContextFactory<FairPlayCombinedDbContext> dbContextFactory =
                 sp.GetRequiredService<IDbContextFactory<FairPlayCombinedDbContext>>();
@@ -40,6 +40,22 @@ namespace FairPlayCombined.Services.Extensions
                 .SingleOrDefault(p => p.Name ==
                 Constants.ConfigurationSecretsKeys.OPENAI_TEXT_GENERATION_MODEL_KEY)
                 ?? throw new InvalidOperationException($"Unable to find {nameof(ConfigurationSecret)} = {Constants.ConfigurationSecretsKeys.OPENAI_TEXT_GENERATION_MODEL_KEY} in database");
+                OpenAIServiceConfiguration openAIServiceConfiguration = new()
+                {
+                    ChatCompletionsUrl = openAIChatCompletionEntity.Value,
+                    GenerateDall3ImageUrl = generateDall3ImageUrlEntity.Value,
+                    Key = openAIKeyEntity.Value,
+                    TextGenerationModel = openAITextGenerationModelEntity.Value
+                };
+                return openAIServiceConfiguration;
+            });
+            builder.Services.AddTransient<IOpenAIService, OpenAIService>(sp =>
+            {
+                OpenAIServiceConfiguration openAIServiceConfiguration = 
+                sp.GetRequiredService<OpenAIServiceConfiguration>();
+
+                IDbContextFactory<FairPlayCombinedDbContext> dbContextFactory =
+                sp.GetRequiredService<IDbContextFactory<FairPlayCombinedDbContext>>();
 
                 var timeoutMinutes = 3;
                 HttpClient openAIAuthorizedHttpClient = new()
@@ -47,7 +63,7 @@ namespace FairPlayCombined.Services.Extensions
                     Timeout = TimeSpan.FromMinutes(timeoutMinutes)
                 };
                 openAIAuthorizedHttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
-                    "Bearer", openAIKeyEntity.Value);
+                    "Bearer", openAIServiceConfiguration.Key);
                 HttpClient genericHttpClient = new()
                 {
                     Timeout = TimeSpan.FromMinutes(timeoutMinutes)
@@ -57,10 +73,10 @@ namespace FairPlayCombined.Services.Extensions
                 return new OpenAIService(openAIAuthorizedHttpClient,
                     genericHttpClient: genericHttpClient, new OpenAIServiceConfiguration()
                     {
-                        Key = openAIKeyEntity.Value,
-                        GenerateDall3ImageUrl = generateDall3ImageUrlEntity.Value,
-                        ChatCompletionsUrl = openAIChatCompletionEntity.Value,
-                        TextGenerationModel = openAITextGenerationModelEntity.Value
+                        Key = openAIServiceConfiguration.Key,
+                        GenerateDall3ImageUrl = openAIServiceConfiguration.GenerateDall3ImageUrl,
+                        ChatCompletionsUrl = openAIServiceConfiguration.ChatCompletionsUrl,
+                        TextGenerationModel = openAIServiceConfiguration.TextGenerationModel
                     },
                 dbContextFactory: dbContextFactory, logger, userProviderService);
             });
