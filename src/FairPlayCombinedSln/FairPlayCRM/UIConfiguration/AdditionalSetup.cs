637146b6ad42890d6679d76392457195ed5a42fa
Eduardo Fonseca <efonsecab@gmail.com>
2024-10-23 05:43:21 -0600

    Applying SonarCloud recommendations

--- src/FairPlayCombinedSln/FairPlayCRM/UIConfiguration/AdditionalSetup.cs
+++ src/FairPlayCombinedSln/FairPlayCRM/UIConfiguration/AdditionalSetup.cs
@@ -5,7 +5,9 @@ namespace FairPlayCRM.UIConfiguration
     public static class AdditionalSetup
     {
         private static readonly Assembly[] additionalAssemblies =
-                [typeof(FairPlayCRM.SharedUI.Components.Pages.Home).Assembly];
+                [
+            typeof(FairPlayCRM.SharedUI.Components.Pages.Home).Assembly,
+        typeof(FairPlayCombined.SharedAuth._Imports).Assembly];
 
         internal static Assembly[] AdditionalAssemblies => additionalAssemblies;
     }
