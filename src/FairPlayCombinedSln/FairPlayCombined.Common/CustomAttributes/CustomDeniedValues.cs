b8f4c3abc7021821c70d1fd0b12ad36cfa60e244
Eduardo Fonseca <efonsecab@gmail.com>
2024-10-26 06:31:09 -0600

    Adding localization to WatchVideo page

---
+++ src/FairPlayCombinedSln/FairPlayCombined.Common/CustomAttributes/CustomDeniedValues.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FairPlayCombined.Common.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public class CustomDeniedValuesAttribute(params object?[] values) : DeniedValuesAttribute(values)
+    {
+        public override string FormatErrorMessage(string name)
+        {
+            return base.FormatErrorMessage(name);
+        }
+    }
+}
