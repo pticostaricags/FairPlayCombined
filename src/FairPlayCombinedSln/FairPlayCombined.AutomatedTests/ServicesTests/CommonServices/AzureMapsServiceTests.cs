a6606418672a9262485c29c92b2b4b508d406da0
Eduardo Fonseca <efonsecab@gmail.com>
2024-11-01 21:47:10 -0600

    Starting to integrate Azure Maps

---
+++ src/FairPlayCombinedSln/FairPlayCombined.AutomatedTests/ServicesTests/CommonServices/AzureMapsServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using FairPlayCombined.Models.AzureMaps;
+using FairPlayCombined.Services.Common;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayCombined.AutomatedTests.ServicesTests.CommonServices
+{
+    [TestClass]
+    public class AzureMapsServiceTests: ServicesBase
+    {
+        [TestMethod]
+        public async Task Test_GetNearbyAirportsAsync()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddUserSecrets<ServicesBase>();
+            var configuration = configurationBuilder.Build();
+            var endpoint = configuration["AzureMaps:Endpoint"] ??
+                throw new Exception("'AzureMaps:Endpoint' is not in configuration");
+            var key = configuration["AzureMaps:Key"] ??
+                throw new Exception("'AzureMaps:Key' is not in configuration");
+            AzureMapsConfiguration azureMapsConfiguration = new()
+            {
+                Endpoint = endpoint,
+                Key = key,
+            };
+            AzureMapsService azureMapsService = new(azureMapsConfiguration,
+                new HttpClient());
+            double latitude = 40.6387345; 
+            double longitude = -73.9529416;
+            var result = await azureMapsService.GetNearbyAirportsAsync(latitude, longitude,
+                CancellationToken.None);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.results!.Length > 0);
+        }
+    }
+}
---
+++ src/FairPlayCombinedSln/FairPlayCombined.Services/Common/AzureMapsService.cs
@@ -0,0 +1,32 @@
+﻿using FairPlayCombined.Models.AzureMaps;
+using System.Net.Http.Json;
+
+namespace FairPlayCombined.Services.Common
+{
+    public class AzureMapsService(AzureMapsConfiguration azureMapsConfiguration,
+        HttpClient httpClient)
+    {
+        /// <summary>
+        /// Check https://learn.microsoft.com/en-us/azure/azure-maps/supported-search-categories
+        /// </summary>
+        public enum POICategories
+        {
+            AIRPORT
+        }
+
+        public async Task<SearchByPOICategoryResponse> GetNearbyAirportsAsync(double latitude, double longitude, CancellationToken cancellationToken)
+        {
+            string format = "json";
+            string query = POICategories.AIRPORT.ToString();
+            string requestUrl = $"{azureMapsConfiguration.Endpoint}" +
+                $"/search/poi/category/{format}" +
+                $"?api-version=1.0" +
+                $"&lat={latitude}" +
+                $"&lon={longitude}" +
+                $"&query={query}" +
+                $"&subscription-key={azureMapsConfiguration.Key}";
+            var result = await httpClient.GetFromJsonAsync<SearchByPOICategoryResponse>(requestUrl, cancellationToken);
+            return result!;
+        }
+    }
+}
