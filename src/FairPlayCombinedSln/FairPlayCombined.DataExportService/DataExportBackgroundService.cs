371405bde333f7ca61d5c9d5d33024e638a66b62
Eduardo Fonseca <efonsecab@gmail.com>
2024-11-11 17:27:26 -0600

    Applying SonarCloud recommendations

--- src/FairPlayCombinedSln/FairPlayCombined.DataExportService/DataExportBackgroundService.cs
+++ src/FairPlayCombinedSln/FairPlayCombined.DataExportService/DataExportBackgroundService.cs
@@ -1,11 +1,9 @@
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
 using FairPlayCombined.DataAccess.Data;
+using FairPlayCombined.DataAccess.Models.dboSchema;
 using FairPlayCombined.Interfaces.FairPlayTube;
 using Microsoft.EntityFrameworkCore;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System.Net.Mail;
 
 namespace FairPlayCombined.DataExportService;
 
@@ -36,32 +34,21 @@ public class DataExportBackgroundService(ILogger<DataExportBackgroundService> lo
                     .AnyAsync(p => !p.IsCompleted, stoppingToken)
                     )
                 {
-                    foreach (var singleEnqueuedItem in dbContext.UserDataExportQueue
-                        .Where(p=>!p.IsCompleted)
-                        .Include(p => p.ApplicationUser))
+                    var queueQuery = dbContext.UserDataExportQueue
+                        .Where(p => !p.IsCompleted)
+                        .Include(p => p.ApplicationUser);
+                    if (await queueQuery.AnyAsync(stoppingToken))
                     {
-                        var fileUrl =
-                        await fairPlayTubeUserDataService.GetUserDataAsync(singleEnqueuedItem.ApplicationUserId, stoppingToken);
-                        singleEnqueuedItem.IsCompleted = true;
-                        singleEnqueuedItem.FileUrl = fileUrl;
-                        dbContext.Database.SetCommandTimeout(TimeSpan.FromMinutes(20));
-                        logger.LogInformation("Saving export into the database");
-                        await dbContext.SaveChangesAsync(stoppingToken);
-                        SendGridMessage sendGridMessage = new()
+                        foreach (var singleEnqueuedItem in queueQuery)
                         {
-                            From = new EmailAddress(emailFrom),
-                            Subject = "FairPlay Data Export",
-                            HtmlContent = "<p>Your data is ready, you can download it from the here:" +
-                            $"<a href='{singleEnqueuedItem.FileUrl}'>{singleEnqueuedItem.FileUrl}</a>.</p>"
-                        };
-
-                        sendGridMessage.AddTo(singleEnqueuedItem.ApplicationUser.Email);
-                        var response =
-                            await sendGridClient.SendEmailAsync(sendGridMessage, stoppingToken);
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            var responseStrong = await response.Body.ReadAsStringAsync(stoppingToken);
-                            logger.LogError("Response: {ResponseMessage}", responseStrong);
+                            var fileUrl =
+                            await fairPlayTubeUserDataService.GetUserDataAsync(singleEnqueuedItem.ApplicationUserId, stoppingToken);
+                            singleEnqueuedItem.IsCompleted = true;
+                            singleEnqueuedItem.FileUrl = fileUrl;
+                            dbContext.Database.SetCommandTimeout(TimeSpan.FromMinutes(20));
+                            logger.LogInformation("Saving export into the database");
+                            await dbContext.SaveChangesAsync(stoppingToken);
+                            await BuildAndSendEmailAsync(logger, emailFrom, sendGridClient, singleEnqueuedItem, stoppingToken);
                         }
                     }
                 }
@@ -71,6 +58,26 @@ public class DataExportBackgroundService(ILogger<DataExportBackgroundService> lo
                 logger.LogError(ex, "Error: {ErrorMessage}", ex.Message);
             }
             await Task.Delay(timeToWait, stoppingToken);
+        }
+    }
+
+    private static async Task BuildAndSendEmailAsync(ILogger<DataExportBackgroundService> logger, string emailFrom, SendGridClient sendGridClient, UserDataExportQueue singleEnqueuedItem, CancellationToken stoppingToken)
+    {
+        SendGridMessage sendGridMessage = new()
+        {
+            From = new EmailAddress(emailFrom),
+            Subject = "FairPlay Data Export",
+            HtmlContent = "<p>Your data is ready, you can download it from the here:" +
+                                        $"<a href='{singleEnqueuedItem.FileUrl}'>{singleEnqueuedItem.FileUrl}</a>.</p>"
+        };
+
+        sendGridMessage.AddTo(singleEnqueuedItem.ApplicationUser.Email);
+        var response =
+            await sendGridClient.SendEmailAsync(sendGridMessage, stoppingToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            var responseStrong = await response.Body.ReadAsStringAsync(stoppingToken);
+            logger.LogError("Response: {ResponseMessage}", responseStrong);
         }
     }
 }
