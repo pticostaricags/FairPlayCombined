8b37d414b2e359009425588b9e936f72e9b8e683
Eduardo Fonseca <efonsecab@gmail.com>
2024-11-12 18:37:44 -0600

    Fixing authorization issue for MAUI app

--- src/FairPlayCombinedSln/FairPlayTube.MAUI/Authentication/CustomAuthenticationStateProvider.cs
+++ src/FairPlayCombinedSln/FairPlayTube.MAUI/Authentication/CustomAuthenticationStateProvider.cs
@@ -1,28 +1,54 @@
 ﻿using FairPlayTube.ClientServices.KiotaClient;
+using FairPlayTube.ClientServices.KiotaClient.Models;
+using FairPlayTube.MAUI.Helpers;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 
 namespace FairPlayTube.MAUI.Authentication
 {
     public class CustomAuthenticationStateProvider(
-        [FromKeyedServices("AuthenticatedApiClient")]
-        ApiClient authenticatedClient
-
+        IServiceProvider serviceProvider,
+        [FromKeyedServices("AnonymousApiClient")]
+        ApiClient anonymousApiClient
         ) : AuthenticationStateProvider
     {
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        private ClaimsPrincipal currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public Task LoginAsync(LoginRequest loginRequest)
         {
-            ClaimsIdentity identity = new();
-            if (UserContext.IsAuthenticated) {
-                var response = await authenticatedClient.Identity.GetMyRoles.GetAsync();
-                foreach (var singleUserRole in response!)
+
+            var loginTask = LogInAsyncCore();
+            NotifyAuthenticationStateChanged(loginTask);
+
+            return loginTask;
+
+            async Task<AuthenticationState> LogInAsyncCore()
+            {
+                var result = await anonymousApiClient!.Login.PostAsync(loginRequest);
+                UserContext.AccessToken = result.AccessToken;
+                UserContext.AccessTokenExpiresIn = result.ExpiresIn;
+                UserContext.RefreshToken = result.RefreshToken;
+                UserContext.TokenExpiraton = DateTimeOffset.UtcNow.AddMinutes(result!.ExpiresIn!.Value);
+                using var scope = serviceProvider.CreateScope();
+                var authenticatedClient = scope.ServiceProvider
+                    .GetRequiredKeyedService<ApiClient>("AuthenticatedApiClient");
+
+                ClaimsIdentity identity = new();
+                if (UserContext.IsAuthenticated)
                 {
-                    identity.AddClaim(new Claim(ClaimTypes.Role, singleUserRole));
+                    var response = await authenticatedClient.Identity.GetMyRoles.GetAsync();
+                    foreach (var singleUserRole in response!)
+                    {
+                        identity.AddClaim(new Claim(ClaimTypes.Role, singleUserRole));
+                    }
                 }
+                this.currentUser = new ClaimsPrincipal(identity);
+                var authenticationState = new AuthenticationState(this.currentUser);
+                return authenticationState;
             }
-            var user = new ClaimsPrincipal(identity);
-            var result = new AuthenticationState(user);
-            return result;
         }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync() =>
+        Task.FromResult(new AuthenticationState(currentUser));
     }
 }
